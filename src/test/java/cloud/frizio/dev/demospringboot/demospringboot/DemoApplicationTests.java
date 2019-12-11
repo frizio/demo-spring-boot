56b4d8477349c43091518d77e918b0b3b20d7f0f
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-12-11 16:21:40 +0100

    first commit

---
+++ src/test/java/cloud/frizio/dev/demospringboot/demospringboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.frizio.dev.demospringboot.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
